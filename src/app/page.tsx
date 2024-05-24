dc0869011ddaf0543692a02905892c59ed23a721
Antonio Guerriere <antonioguerriere@gmail.com>
2024-05-24 22:50:52 +0100

    fixed text center temporary site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-[#C52628] h-screen text-[#f7be84] flex flex-col justify-center items-center">
+    <main className="bg-[#C52628] h-screen text-[#f7be84] flex flex-col justify-center items-center text-center">
       <h1 className="text-2xl lg:text-7xl mb-5">A McHnery Construction Ltd</h1>
       <h5 className="text-xl lg:text-5xl mb-5">New website coming June 2024</h5>
       <h5 className="text-xl lg:text-5xl">To arrange a house viewing call Shane: 07894401453</h5>

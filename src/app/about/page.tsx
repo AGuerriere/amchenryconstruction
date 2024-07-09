dfe3aeacc1eb0099f9f20b55eb547983752df93c
Antonio Guerriere <antonioguerriere@gmail.com>
2024-07-09 20:18:10 +0200

    complegted about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,19 +17,26 @@ export default function About() {
               <h2 className="md:col-span-2 lg:col-span-1 text-gray-900 tracking-wide mb-5 overflow-hidden text-ellipsis">Our Story</h2>
               <div className="lg:col-span-2">
                 <p className="mb-5">
-                At A McHenry Construction, a family-run business based in Ballycastle, Northern Ireland, we take pride in our over 40 years of experience in the construction industry, earning a reputation for excellence, quality, and integrity. 
+                  At A McHenry Construction, a family-run business based in Ballycastle, Northern Ireland, we take pride in our over 40 years of experience in the construction industry, earning a reputation for excellence, quality, and integrity.
                 </p>
                 <p>
-                Founded in 1999 by Alastair McHenry, our company is now proudly led by his sons, Shane and Mark, who continue to uphold the high standards and values that have been the cornerstone of our success.                 </p>
+                  Founded in 1999 by Alastair McHenry, our company is now proudly led by his sons, Shane and Mark, who continue to uphold the high standards and values that have been the cornerstone of our success.                 </p>
               </div>
               <div className="lg:col-span-2">
                 <p className="mb-5">
-                Alastair&apos;s vision was to create homes that not only meet but exceed client expectations, and his commitment to quality craftsmanship and attention to detail quickly set us apart in our industry.                </p>
+                  Alastair&apos;s vision was to create homes that not only meet but exceed client expectations, and his commitment to quality craftsmanship and attention to detail quickly set us apart in our industry.                </p>
                 <p>
-                Today, Shane and Mark honour this legacy by integrating innovative ideas and modern techniques into our projects while maintaining the traditional values that have always defined our work.                </p>
+                  Today, Shane and Mark honour this legacy by integrating innovative ideas and modern techniques into our projects while maintaining the traditional values that have always defined our work.                </p>
                 <div className="flex justify-start mt-10">
                   <Image src="/images/nhbc.webp" alt="NHBC" width={100} height={50} />
                 </div>
+              </div>
+            </div>
+            <div className="w-full bg-[url('/images/colorful-houses.webp')] bg-cover bg-no-repeat bg-center min-h-fit mb-10">
+              <div className="bg-green-gradient h-full min-h-[430px] p-10 flex flex-col justify-center items-center text-center">
+                <h2 className="text-white mb-5">Our <span className="text-gray-900">Team</span></h2>
+                <p className="text-white mb-5">Our team is comprised of skilled professionals who are passionate about what they do. From architects and designers to builders and project managers, everyone at A McHenry  Construction shares a common goal: to deliver exceptional homes that our clients will love for years to come. We believe in fostering a collaborative environment where every team  member's expertise contributes to the success of each project.</p>
+
               </div>
             </div>
             <Contacts />

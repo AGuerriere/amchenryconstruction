f25a689bc5d479eab70e8a6cf0e742f2646f88f9
Antonio Guerriere <antonioguerriere@gmail.com>
2024-07-14 17:04:06 +0200

    improved metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ const inter = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'A McHenry Construction',
-  description: 'A McHenry Construction, construction company. Building outstanding new developments since 1999',
+  description: 'A McHenry Construction, is a construction company based in Ballycastle. Building outstanding new developments for over 40 years.',
   keywords: [
     'Construction Company', 'Building Services', 'Construction Services', 'Residential Construction', 'Commercial Construction',
     'Property Development', 'Construction Projects', 'Ballycastle Construction', 'Northern Ireland Builders', 'Construction Management',
-    'Renovation Services', 'Building Renovation', 'Home Extensions', 'New Build Homes', 'Civil Engineering', 'Architectural Services',
+    'Renovation Services', 'Building Renovation', 'Home Extensions', 'New Build Homes', 'New Builds Ballycastle', 'house builders', 'Civil Engineering', 'Architectural Services',
     'Project Management', 'Sustainable Construction', 'Construction Consultancy', 'Ballycastle', 'North Coast', 'Causeway Coast',
     'Antrim Coast', 'Northern Ireland', 'NI Construction', 'Construction Expertise', 'Property Maintenance', 'Custom Builds',
     'Eco-Friendly Building', 'Land Development', 'Construction Planning', 'Building Regulations', 'Construction Safety'
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: '/images/logo.webp',
     title: 'A McHenry Construction',
-    description: 'A McHenry Construction, construction company. Building outstanding new developments since 1999',
+    description: 'A McHenry Construction, is a construction company based in Ballycastle. Building outstanding new developments for over 40 years.',
     url: 'https://amchenryconstruction.com/',
     siteName: 'A McHenry Construction',
   },
